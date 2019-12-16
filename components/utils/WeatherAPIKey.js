84b9aeec29f1885462cd733867f2cac1b3daeec5
Sithija Shehara <nsithijashehara@gmail.com>
2019-12-16 23:53:05 +0530

    first commit

---
+++ components/utils/WeatherAPIKey.js
@@ -0,0 +1,23 @@
+
+export const API_KEY = '678cdeb99c8dd0704087dbea4ed84375';
+
+// API key:
+// - Your API key is 678cdeb99c8dd0704087dbea4ed84375
+// - Within the next couple of hours, it will be activated and ready to use
+// - You can later create more API keys on your account page
+// - Please, always use your API key in each API call
+
+// Endpoint:
+// - Please, use the endpoint api.openweathermap.org for your API calls
+// - Example of API call:
+// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=678cdeb99c8dd0704087dbea4ed84375
+
+// Useful links:
+// - API documentation https://openweathermap.org/api
+// - Details of your plan https://openweathermap.org/price
+// - Please, note that 16-days daily forecast and History API are not available for Free subscribers
+
+
+// Blog
+// Support center & FAQ
+// Contact us info@openweathermap.org.
